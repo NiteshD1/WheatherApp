b988006a480ca4be9f9d858d5bf0253f4e625780
Nitesh Dewada <devadanitesh@gmail.com>
2021-05-19 15:24:08 +0530

    Initial commit

---
+++ app/src/main/java/com/studyquiz/wheatherapp/activities/WheatherActivity.kt
@@ -0,0 +1,14 @@
+package com.studyquiz.wheatherapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.studyquiz.wheatherapp.R
+
+class WheatherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wheather)
+
+
+    }
+}
